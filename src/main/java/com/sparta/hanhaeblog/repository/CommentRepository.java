154121b1d043805def5943c9348b7195368622af
limseulki <limslki333@hanmail.net>
2023-04-20 21:15:54 +0900

    댓글 작성날짜 순으로 내림차순 정렬 구현

--- src/main/java/com/sparta/hanhaeblog/repository/CommentRepository.java
+++ src/main/java/com/sparta/hanhaeblog/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package com.sparta.hanhaeblog.repository;
 import com.sparta.hanhaeblog.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByIdOrderByCreatedAtDesc(Long id);
 }

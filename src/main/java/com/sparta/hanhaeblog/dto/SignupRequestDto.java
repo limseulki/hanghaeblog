30b9b7a9b88f0d7ac7e418294a029fe71c8a09bc
limseulki <limslki333@hanmail.net>
2023-04-21 09:47:14 +0900

    정규표현식 수정

--- src/main/java/com/sparta/hanhaeblog/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/hanhaeblog/dto/SignupRequestDto.java
@@ -13,13 +13,13 @@ public class SignupRequestDto {
     // 최소 4자 이상, 10자 이하
     // username은 알파벳 소문자, 숫자
     @Size(min = 4, max = 10)
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z]).{4,10}$", message = "아이디는 4~10자 알파벳 소문자, 숫자로 작성해주세요.")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z]).{4,10}$", message = "아이디는 4~10자 알파벳 소문자, 숫자로 작성해주세요.")
     private String username;
 
     // 최소 8자 이상, 15자 이하
     // password는 알파벳 대소문자, 숫자
     @Size(min = 8, max = 15)
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,15}$", message = "비밀번호는 8~15자 알파벳 대소문자, 숫자, 특수문자로 작성해주세요.")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])(?=.*[~!@#$%^&*()_+|<>?:{}])(?=\\S+$).{8,15}$", message = "비밀번호는 8~15자 알파벳 대소문자, 숫자, 특수문자로 작성해주세요.")
     private String password;
 
     private boolean admin = false;

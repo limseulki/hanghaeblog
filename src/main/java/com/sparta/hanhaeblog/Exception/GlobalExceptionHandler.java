5b224b4ca43a52bf1c28549656a7c661fad96f10
limseulki <limslki333@hanmail.net>
2023-04-24 21:58:49 +0900

    Valid 예외 핸들러 추가

--- src/main/java/com/sparta/hanhaeblog/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/hanhaeblog/Exception/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sparta.hanhaeblog.Exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
@@ -14,5 +17,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
         return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+
+    // Valid 예외 핸들러 (아이디 패스워드 유효성 검사)
+    @ExceptionHandler({BindException.class})
+    public ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+        BindingResult bindingResult = e.getBindingResult();
+
+        StringBuilder sb = new StringBuilder();
+        for ( FieldError fieldError : bindingResult.getFieldErrors()) {
+            sb.append(fieldError.getDefaultMessage());
+        }
+        return ErrorResponse.toResponseEntityValid(sb.toString(), HttpStatus.BAD_REQUEST);
     }
 }
